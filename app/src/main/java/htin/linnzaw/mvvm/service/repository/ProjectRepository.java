5805a971d8cc05986932c2c13475422cb0d4e56b
Htin Linn Zaw <p-aung@isgm2.com>
2020-01-07 13:49:32 +0630

    ProjectRepository

--- app/src/main/java/htin/linnzaw/mvvm/service/repository/ProjectRepository.java
+++ app/src/main/java/htin/linnzaw/mvvm/service/repository/ProjectRepository.java
@@ -1,4 +1,38 @@
 package htin.linnzaw.mvvm.service.repository;
 
-public class ProjectRepository {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.List;
+
+import htin.linnzaw.mvvm.service.model.Project;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ProjectRepository
+{
+    private GithubService githubservice;
+
+    public LiveData<List<Project>> getProjectList(String userId)
+    {
+        final MutableLiveData<List<Project>> data = new MutableLiveData<>();
+
+        githubservice.getProjectList(userId).enqueue(new Callback<List<Project>>()
+        {
+            @Override
+            public void onResponse(Call<List<Project>> call, Response<List<Project>> response)
+            {
+                data.setValue(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Project>> call, Throwable throwable)
+            {
+
+            }
+        });
+
+        return data;
+    }
 }
