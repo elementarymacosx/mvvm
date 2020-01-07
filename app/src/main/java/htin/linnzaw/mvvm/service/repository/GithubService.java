f34f59addb7f9ad6950f55dea485b02f41d1a77f
Htin Linn Zaw <p-aung@isgm2.com>
2020-01-07 13:42:47 +0630

    GitHub Service

--- app/src/main/java/htin/linnzaw/mvvm/service/repository/GithubService.java
+++ app/src/main/java/htin/linnzaw/mvvm/service/repository/GithubService.java
@@ -1,6 +1,19 @@
 package htin.linnzaw.mvvm.service.repository;
 
-public class GithubService
-{
+import java.util.List;
 
+import htin.linnzaw.mvvm.service.model.Project;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface GithubService
+{
+    String HTTPS_Api_Github_Url = "https://api.github.com/";
+
+    @GET("users/{user}/repos")
+    Call<List<Project>> getProjectList(@Path("user") String user);
+
+    @GET("/repos/{user}/{reponame}")
+    Call<Project> getProjectDetails(@Path("user") String user, @Path("reponame") String projectName);
 }
